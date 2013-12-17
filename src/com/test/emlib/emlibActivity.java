eda732fbc7df9f94c6737ac9195ebd8bd5bdb337
Pranay Kumar <pkumar6@iit.edu>
2013-12-17 13:04:13 -0600

    This is a test android poject

---
+++ src/com/test/emlib/emlibActivity.java
@@ -0,0 +1,15 @@
+package com.test.emlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class emlibActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
